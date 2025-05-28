8b435e121ca6a36a6ce5a378446c464c9f4e229a
marinlamy <marin.lamy@epitech.eu>
2025-05-28 14:01:44 +0200

    [FEAT](network): Add NetworkThread squelette class for managing network operations

---
+++ src/GUI/network/networkThread/NetworkThread.cpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** networkThread
+*/
+
+#include "NetworkThread.hpp"
+
---
+++ src/GUI/network/networkThread/NetworkThread.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** networkThread
+*/
+
+#ifndef NETWORKTHREAD_HPP_
+#define NETWORKTHREAD_HPP_
+
+#include <thread>
+#include <functional>
+#include <atomic>
+
+class NetworkThread {
+
+};
+#endif /* !NETWORKTHREAD_HPP_ */
