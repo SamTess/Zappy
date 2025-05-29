d3afed6466e54e541e00bc383f1350f5a35846f0
marinlamy <marin.lamy@epitech.eu>
2025-05-29 12:48:39 +0200

    [FIX}(Player): Added null checks for player and inventory in inventory management functions

--- src/Server/player/player_inventory_utils.c
+++ src/Server/player/player_inventory_utils.c
@@ -33,6 +33,8 @@ char *get_resource_name(ResourceType_t type)
 
 bool inventory_has_item(player_t *player, ResourceType_t type)
 {
+    if (!player || !player->inventory)
+        return false;
     for (int i = 0; i < player->inventory_size; i++) {
         if (player->inventory[i].type == type) {
             return true;
@@ -43,6 +45,8 @@ bool inventory_has_item(player_t *player, ResourceType_t type)
 
 int how_many_in_inventory(player_t *player, ResourceType_t type)
 {
+    if (!player)
+        return 0;
     for (int i = 0; i < player->inventory_size; i++) {
         if (player->inventory[i].type == type) {
             return player->inventory[i].quantity;
