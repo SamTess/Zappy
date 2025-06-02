f24b281e9b46fa9bf51b9f08d22493aceaab5563
marinlamy <marin.lamy@epitech.eu>
2025-06-02 19:43:36 +0200

    [REFAC] (resource): Standardized resource_type naming convention across the codebase

--- src/Server/command/take_object.c
+++ src/Server/command/take_object.c
@@ -11,38 +11,36 @@
 #include <unistd.h>
 #include <string.h>
 
-ResourceType_t determine_type(char *cut_string)
+resource_type_t determine_type(char *resource_string)
 {
-    if (strncmp(cut_string, "food", 4) == 0)
+    if (strncmp(resource_string, "food", 4) == 0)
         return FOOD;
-    if (strncmp(cut_string, "linemate", 8) == 0)
+    if (strncmp(resource_string, "linemate", 8) == 0)
         return LINEMATE;
-    if (strncmp(cut_string, "deraumere", 9) == 0)
+    if (strncmp(resource_string, "deraumere", 9) == 0)
         return DERAUMERE;
-    if (strncmp(cut_string, "sibur", 5) == 0)
+    if (strncmp(resource_string, "sibur", 5) == 0)
         return SIBUR;
-    if (strncmp(cut_string, "mendiane", 8) == 0)
+    if (strncmp(resource_string, "mendiane", 8) == 0)
         return MENDIANE;
-    if (strncmp(cut_string, "phiras", 6) == 0)
+    if (strncmp(resource_string, "phiras", 6) == 0)
         return PHIRAS;
     return COUNT;
 }
 
 void take_object(server_t *server, client_t *client, char *buffer)
 {
-    char *cut_string = buffer + 5;
-    ResourceType_t resource_type = determine_type(cut_string);
+    char *resource_name = buffer + 5;
+    resource_type_t resource_type = determine_type(resource_name);
 
-    if (client == NULL || client->player == NULL) {
+    if (client == NULL || client->player == NULL || resource_type == COUNT) {
         write_command_output(client->client_fd, "ko\n");
         return;
     }
-    if (resource_type == COUNT) {
-        write_command_output(client->client_fd, "ko\n");
-        return;
-    }
-    if (server->map[client->player->pos_y][client->player->pos_x].resources[resource_type] > 0) {
-        server->map[client->player->pos_y][client->player->pos_x].resources[resource_type]--;
+    if (server->map[client->player->pos_y]
+        [client->player->pos_x].resources[resource_type] > 0) {
+        server->map[client->player->pos_y]
+        [client->player->pos_x].resources[resource_type]--;
         add_item_to_inventory(client->player, resource_type, 1);
         write_command_output(client->client_fd, "ok\n");
     } else {
