3dcf47a3051f30cf8d7d04209d6b03df275bafac
ctalneau <clement.talneau@epitech.eu>
2025-06-03 14:45:33 +0200

    [FIX](Left, Right): Fixed bug in left and right command

--- src/Server/command/right.c
+++ src/Server/command/right.c
@@ -7,6 +7,26 @@
 #include "../include/command.h"
 #include <stdio.h>
 
+static void change_rot(client_t *client)
+{
+    switch (client->player->rotation) {
+    case UP:
+        client->player->rotation = RIGHT;
+        break;
+    case DOWN:
+        client->player->rotation = LEFT;
+        break;
+    case LEFT:
+        client->player->rotation = UP;
+        break;
+    case RIGHT:
+        client->player->rotation = DOWN;
+        break;
+    default:
+        break;
+    }
+}
+
 void right(server_t *server, client_t *client, char *buffer)
 {
     (void)server;
@@ -18,13 +38,6 @@ void right(server_t *server, client_t *client, char *buffer)
         write_command_output(client->client_fd, "ko\n");
         return;
     }
-    if (client->player->rotation == UP)
-        client->player->rotation = RIGHT;
-    if (client->player->rotation == DOWN)
-        client->player->rotation = LEFT;
-    if (client->player->rotation == LEFT)
-        client->player->rotation = UP;
-    if (client->player->rotation == RIGHT)
-        client->player->rotation = DOWN;
+    change_rot(client);
     write_command_output(client->client_fd, "ok\n");
 }
