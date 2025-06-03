301c80014b6998439682f68ff34b3ebfb1d70053
marinlamy <marin.lamy@epitech.eu>
2025-06-03 09:10:44 +0200

    [FIX] (command): Corrected resource_type initialization in set_object and take_object functions

--- src/Server/command/set_object.c
+++ src/Server/command/set_object.c
@@ -7,15 +7,17 @@
 
 #include "../include/command.h"
 #include <stdio.h>
+#include <string.h>
 
 void set_object(server_t *server, client_t *client, char *buffer)
 {
     char *resource_name;
-    resource_type_t resource_type = determine_type(resource_name);
+    resource_type_t resource_type;
 
         if (strlen(buffer) <= 4)
         return write_command_output(client->client_fd, "ko\n");
     resource_name = buffer + 4;
+    resource_type = determine_type(resource_name);
     if (client == NULL || client->player == NULL || resource_type == COUNT)
         return write_command_output(client->client_fd, "ko\n");
     if (how_many_in_inventory(client->player, resource_type) <= 0)
--- src/Server/command/take_object.c
+++ src/Server/command/take_object.c
@@ -31,11 +31,12 @@ resource_type_t determine_type(char *resource_string)
 void take_object(server_t *server, client_t *client, char *buffer)
 {
     char *resource_name;
-    resource_type_t resource_type = determine_type(resource_name);
+    resource_type_t resource_type;
 
     if (strlen(buffer) <= 5)
         return write_command_output(client->client_fd, "ko\n");
     resource_name = buffer + 5;
+    resource_type = determine_type(resource_name);
     if (client == NULL || client->player == NULL || resource_type == COUNT)
         return write_command_output(client->client_fd, "ko\n");
     if (server->map[client->player->pos_y]
