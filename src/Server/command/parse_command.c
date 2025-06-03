83d2860916251bcda20308bffc18a2733935cc51
marinlamy <marin.lamy@epitech.eu>
2025-06-03 18:46:09 +0200

    [ADD](Command Queue): Implemented command queue management with functions to add commands and process the next command in the queue.

--- src/Server/command/parse_command.c
+++ src/Server/command/parse_command.c
@@ -56,12 +56,13 @@ static int check_buffer_size(int b_size, char *buffer, client_t *user)
 static command_data_t get_command_data(void)
 {
     static const char *comm_char[] = {"Forward", "Right", "Left",
-        "Inventory", "Look", "Eject", "Connect_nbr",
-        "Take", "Set", "Fork", NULL};
+        "Inventory", "Look", "Eject", "Connect_nbr", "Take", "Set",
+        "Incantation", "Fork", NULL};
     static void (*comm_func[])(server_t *, client_t *, char *) =
         {forward, right, left, inventory, look, eject,
-            connect_nbr, take_object, set_object, fork_c, NULL};
-    static int comm_times[] = {7, 7, 7, 1, 7, 7, 0, 7, 7, 42};
+        connect_nbr, take_object, set_object, start_incantation,
+        fork_c, NULL};
+    static int comm_times[] = {7, 7, 7, 1, 7, 7, 0, 7, 7, 300, 42};
     command_data_t data = {comm_char, comm_func, comm_times};
 
     return data;
