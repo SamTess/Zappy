1af3b14bbec90e4ab536b200b98ec04e14ffb262
marinlamy <marin.lamy@epitech.eu>
2025-06-09 12:24:12 +0200

    [FEAT] (graphicalCommand): Implement graphical commands mzs

---
+++ src/Server/graphical_command/command_msz.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** command_msz
+*/
+
+#include "../include/server.h"
+#include "../include/client.h"
+#include "../include/command.h"
+#include "../include/graphical_commands.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void send_msz_command(server_t *server, client_t *client)
+{
+    char *buffer = NULL;
+    int size = 0;
+
+    if (!server || !client)
+        return;
+    buffer = NULL;
+    size = snprintf(NULL, 0, "msz %d %d\n",
+            server->parsed_info->width, server->parsed_info->height);
+    buffer = malloc(size + 1);
+    if (!buffer)
+        return;
+    snprintf(buffer, size + 1, "msz %d %d\n",
+            server->parsed_info->width, server->parsed_info->height);
+    write_command_output(client->client_fd, buffer);
+    free(buffer);
+}
