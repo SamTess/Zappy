d94bcd517c0d48e730e931d998237c3f1f264ca3
ctalneau <clement.talneau@epitech.eu>
2025-06-11 11:40:11 +0200

    [FIX](Ebo command): Fixed memory allocation in ebo command

--- src/Server/graphical_command/command_ebo.c
+++ src/Server/graphical_command/command_ebo.c
@@ -13,13 +13,18 @@
 void send_ebo_command(server_t *server, int egg_id)
 {
     graphical_client_t *current = NULL;
-    char buffer[32];
-    if (!server && !server->graphical_clients)
+    char *buffer;
+    int size;
+
+    if (!server || !server->graphical_clients)
         return;
+    size = snprintf(NULL, 0, "ebo #%d\n", egg_id);
+    buffer = malloc(size + 1);
     snprintf(buffer, sizeof(buffer), "ebo #%d\n", egg_id);
     current = server->graphical_clients;
     while (current) {
         write_command_output(current->client->client_fd, buffer);
         current = current->next;
     }
+    free(buffer);
 }
