7c2c8f0a993c1704e2d62f1a8bdeefc7ae743010
marinlamy <marin.lamy@epitech.eu>
2025-06-12 13:10:27 +0200

    [REFAC] (right command): Remove unused parameters from right function for cleaner code

--- src/Server/command/right.c
+++ src/Server/command/right.c
@@ -31,8 +31,7 @@ static void change_rot(client_t *client)
 
 void right(server_t *server, client_t *client, char **buffer)
 {
-    (void)server;
-    (void)buffer;
+
     if (!server || !client || !client->player || arr_len(buffer) != 1)
         return write_command_output(client->client_fd, "ko\n");
     if (client->player->rotation != UP && client->player->rotation != DOWN
