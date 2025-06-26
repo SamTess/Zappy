f72a411e7ce6637666409507d44176919b7a7bdc
Samuel Tesson <samuel.tesson@epitech.eu>
2025-06-26 19:06:38 +0200

    [FEAT] (IUISettingsProvider): Add interface for accessing UI settings

---
+++ src/GUI/ui/data/IUISettingsProvider.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** Interface for UI settings access
+*/
+
+#ifndef IUI_SETTINGS_PROVIDER_HPP_
+#define IUI_SETTINGS_PROVIDER_HPP_
+
+namespace GUI {
+
+class IUISettingsProvider {
+public:
+    virtual ~IUISettingsProvider() = default;
+
+    virtual bool getSfxEnabled() const = 0;
+    virtual float getMusicVolume() const = 0;
+    
+    virtual void setSfxEnabled(bool enabled) = 0;
+    virtual void setMusicVolume(float volume) = 0;
+};
+
+} // namespace GUI
+
+#endif /* !IUI_SETTINGS_PROVIDER_HPP_ */
