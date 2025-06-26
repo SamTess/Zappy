3d14d65db58703ca44e63a3ff2bdf7175f1e6b9d
Samuel Tesson <samuel.tesson@epitech.eu>
2025-06-26 15:08:18 +0200

    [REFAC](GameController): Create SoundManager class for better separation for the sound part of the GUI

---
+++ src/GUI/gameController/soundManager/SoundManager.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** SoundManager
+*/
+
+#include <string>
+#include "SoundManager.hpp"
+
+SoundManager::SoundManager(std::shared_ptr<IGraphicsLib> graphics, std::shared_ptr<GameState> gameState)
+    : _graphics(graphics), _gameState(gameState) {}
+
+void SoundManager::playDeathSound() {
+    if (_graphics) {
+        _graphics->PlaySound("assets/music/death.mp3");
+    }
+}
+
+void SoundManager::playBroadcastSound() {
+    if (_graphics) {
+        _graphics->PlaySound("assets/music/discord.mp3");
+    }
+}
+
+void SoundManager::playSound(const std::string& soundPath) {
+    if (_graphics)
+        _graphics->PlaySound(soundPath);
+}
+bool SoundManager::isSfxEnabled() const {
+    if (!_gameState)
+        return false;
+    return _gameState->getSfxEnabled();
+}
---
+++ src/GUI/gameController/soundManager/SoundManager.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** SoundManager
+*/
+
+#ifndef SOUND_MANAGER_HPP_
+#define SOUND_MANAGER_HPP_
+
+#include <memory>
+#include <string>
+#include "../../../Shared/IGraphicsLib.hpp"
+#include "../GameState.hpp"
+
+class SoundManager {
+public:
+    SoundManager(std::shared_ptr<IGraphicsLib> graphics, std::shared_ptr<GameState> gameState);
+    ~SoundManager() = default;
+
+    void playDeathSound();
+    void playBroadcastSound();
+    void playSound(const std::string& soundPath);
+    bool isSfxEnabled() const;
+
+private:
+    std::shared_ptr<IGraphicsLib> _graphics;
+    std::shared_ptr<GameState> _gameState;
+};
+
+#endif /* !SOUND_MANAGER_HPP_ */
